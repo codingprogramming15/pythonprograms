70f9b3f182580613aa991e0396d15bf77368b6b7
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:01:01 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+# d={}
+#     print(type(d))
+# d={1:'abc',22:'sonu','pythonlife':1}
+# print(d[22])
+
+d={1:'abc',22:'sonu','pythonlife':1}
+# print(d.get(1))
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# d.update{(1111:4444)}
+# print(d)
+
+for i,j in {1:'abc',22:'sonu','pythonlife':1}.items():
+    print(i,j)

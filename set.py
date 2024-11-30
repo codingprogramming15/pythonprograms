3fcb3c9b25e996d78a76aa5ba1332f45196ae84b
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:02:39 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,41 @@
+s={1,1134,5}
+print(type(s))
+
+
+s={11,55,67,8,2,9,2,34,0,76}
+'''
+add
+update
+pop
+remove
+
+'''
+#print(s)
+#s.add(123)
+
+#s.update((1,2,3,45))
+
+#s.pop
+#s.remove(34)
+#print(s)
+
+
+'''
+union
+intersection
+deference
+is subset
+is superset
+
+'''
+set1={1,2,3}
+set2={4,5,6}
+#print(set1.union(set2))
+#print(set1.intersection(set2))
+#print(set1.issubset(set2))
+#print(set1.issuperset(set2))
+#print(set1.difference(set2))
+
+
+for i in(1,2,3,4):
+    print(i)

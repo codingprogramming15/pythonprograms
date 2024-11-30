72cd1e43ba6ed6370c81ff94ea99a31e3d109c51
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:02:05 +0530

    Add files via upload

---
+++ looping statements.py
@@ -0,0 +1,37 @@
+''' for
+    while
+    nested loop
+    '''
+    
+# for i in range(1,10,2):
+#     print(i)
+
+
+# a=[2,2,2,4,5,55,77,66,678,9876]
+# for i in a:
+#     print(i)
+
+
+# a="pythonprogram"
+# for i in a:
+#     print(i)
+
+
+# while True:
+#     print("hiii")
+
+# fareena=9
+# while fareena<15:
+#     print("hii",fareena)
+#     fareena+=1
+
+# for i in range(0,10):
+#     for j in range(0,100):
+#         print(i+j)
+    
+    
+    
+    
+    
+
+    

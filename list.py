d3698f7c55632777f7b642782ab137355d913a28
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:01:47 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,38 @@
+# v=[]
+# print(type(v))
+
+# v=[1,3,45,6787,9633,78,"farru"]
+# print(v)
+# print(v[-1])
+# print(v[0:4:2])
+
+''' 
+append
+extend
+insert
+pop
+remove
+count
+'''
+# v=[1,3,45,6787,9633,78,"farru"]
+# v.append("python")
+# print(v)
+
+# v=[1,3,45,6787,9633,78,"farru"]
+# v.extend("python")
+# print(v)
+
+# v=[1,3,45,6787,9633,78,"farru"]
+# print(v.count("1"))
+# v.remove(45)
+# v.pop(55)
+# print(v)
+# print(v.index(45))
+
+
+# for i in [1,3,67,867,97648,"sonu"]:
+#     print(i)
+
+v=[1,3,45,6787,9633,78,"farru"]
+v.insert(0,"xyz")
+print(v)

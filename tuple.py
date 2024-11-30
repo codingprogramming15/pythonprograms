13d4e342cedec79a4cbe005bf9e3942742385a44
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:03:11 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,29 @@
+# c=()
+# print(type(c))
+
+
+# c=(1,34,56,567,43)
+# print(c[-1])
+# print(c[0:3:2])
+
+# c=(1,34,56,567,43)
+# print(min(c))
+# print(max(c))
+# print(sum(c))
+# print(len(c))
+
+# t1=(1,2,3)
+# t2=(4,5,6)
+# print(t1+t2)
+
+
+# c=(1,34,56,567,43)
+# print(c*11)
+# for i in c:
+#     print(i)
+
+# print(11  not in c)
+
+t1=(1,2,3)
+t2=(4,5,6)
+print(t1 is t2)

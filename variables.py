cfa4821858b2d38467a9f0c4911ffe4b7ea7c03e
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:03:25 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,28 @@
+#variable=value
+
+
+#a=10
+#print(a)
+
+#cannot use multiple variables single value
+
+a,b,c =1
+print(a,b,c)
+
+C=1,2,2
+print(C)
+
+
+c=v=x=22
+print(v)
+
+_abc=3
+print(_abc)
+
+abc4343=5
+print(id(abc4343))
+
+pythonlife=5
+PYTHONLIFE=15
+print(pythonlife)
+

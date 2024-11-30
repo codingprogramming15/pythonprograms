19c14d60c1b27a70d46da68021209038da04373a
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:01:32 +0530

    Add files via upload

---
+++ jumpingstatements.py
@@ -0,0 +1,28 @@
+'''
+pass 
+break
+continue
+
+'''
+
+# if True:
+#     pass
+
+# for i in "pythonlife":
+#     if i=='1':
+#         break
+#     print(i)
+
+for i in "pythonlife":
+    if i=='1':
+        continue
+    print(i)
+    
+    ''' 
+    pass--->ignore
+    break---->terminate
+    continue--->skip(current iterated)
+    
+    '''
+    
+    

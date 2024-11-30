7f9ad79766dce84cc7108b98de9e2968a6a60ea2
codingprogramming15 <141837563+codingprogramming15@users.noreply.github.com>
2024-11-30 13:00:29 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,47 @@
+''' decision making statements
+if
+else
+elif
+nested if
+
+'''
+
+'''
+syntax:
+if condition:
+    statements
+else:
+    statements    
+    '''
+# if True:
+#     print("my if")
+# elif True:
+#     print("my elif")    
+# else:
+#     print("my else") 
+    
+    
+# if False:
+#     print("outer if")  
+#     if False:
+#         print("inner if")    
+#     else:
+#         print("inner else")  
+# else:
+#     print("outer else")      
+
+
+# age=18
+# if age>18:
+#     print("you can vote")
+# elif age==18:
+#     print("you can vote buddy")    
+# else:
+#     print("wait")    
+
+
+age=18
+if age>18 or age==18:
+     print("you can vote")
+else:
+    print("wait")     
